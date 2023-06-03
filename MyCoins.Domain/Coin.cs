9931f68b988b1c64429d5a6a3260ce22757aaa3b
Alexey Vasilchenko <vasillesha55@gmail.com>
2023-06-03 19:44:04 +0300

    first commit

---
+++ MyCoins.Domain/Coin.cs
@@ -0,0 +1,12 @@
+﻿namespace MyCoins.Domain
+{
+    public class Coin
+    {
+        public Guid Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Guid CoinType { get; set; }
+        public Guid UserId { get; set; }
+        public string? Description { get; set; }
+    }
+}
