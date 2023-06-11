61de0c2a6cea074b765ed5570b329261e64fb498
Alexey Vasilchenko <vasillesha55@gmail.com>
2023-06-11 13:42:02 +0300

    Update GetTransationListQuery

--- MyCoins.Application/Entities/Transactions/Queries/GetTransationList/GetTransationListQuery.cs
+++ MyCoins.Application/Entities/Transactions/Queries/GetTransationList/GetTransationListQuery.cs
@@ -4,6 +4,8 @@ namespace MyCoins.Application.Entities.Transactions.Queries.GetTransationList
 {
     public class GetTransationListQuery : IRequest<TransationListVm>
     {
+        public Guid UserId { get; set; }
+
         public class GetTransationListQueryHandler : IRequestHandler<GetTransationListQuery, TransationListVm>
         {
             public Task<TransationListVm> Handle(GetTransationListQuery request, CancellationToken cancellationToken)
